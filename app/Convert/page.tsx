c3723f585b05a2e62a1fe70dc225298200e8adec
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-01-16 23:08:16 +0530

    more convert page changes

--- app/Convert/page.tsx
+++ app/Convert/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Page(){
     return(
-        <div className="z-20 text-black flex flex-col gap-2">
+        <div className="z-20 absolute text-black flex flex-col gap-2">
             Convert page
             <Link href='/Convert/Mp4-to-Gif'>MP4 to Gif</Link>
             <Link href='/Convert/Mp4-to-Mp3'>MP4 to MP3</Link>
