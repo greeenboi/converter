3a3c8015f47ef65de3d731d075d83c9a03b80418
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-01-16 23:03:39 +0530

    added placeholder page for convert

---
+++ app/Convert/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page(){
+    return(
+        <div>
+            Convert page
+            <Link href='/Convert/Mp4-to-Gif'>MP4 to Gif</Link>
+            <Link href='/Convert/Mp4-to-Mp3'>MP4 to MP3</Link>
+        </div>
+    )
+}
